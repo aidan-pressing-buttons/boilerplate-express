d967e8fd083aa9c748bbb498628787aa389ff57c
Aidan Madden <aidanmadden22+github@gmail.com>
2024-06-29 21:55:17 +0000

    add sendFile

--- myApp.js
+++ myApp.js
@@ -4,9 +4,10 @@ let app = express();
 console.log("Hello World")
 
 app.get("/", hello)
+absolutePath = __dirname + '/views/index.html'
 
 function hello(req, res) {
-    res.send('Hello Express')
+    res.sendFile(absolutePath)
 }
 
 
