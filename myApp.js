c317de7bc1520328ec16dfb6658ccc28c6510d04
Aidan Madden <aidanmadden22+github@gmail.com>
2024-06-29 23:27:42 +0000

    env variable handling

--- myApp.js
+++ myApp.js
@@ -1,14 +1,22 @@
+require('dotenv').config()
 let express = require('express');
 const { log } = require('fcc-express-bground');
 let app = express();
 absolutePath = __dirname + '/views/index.html'
 middlewarePath = __dirname + '/public'
 console.log("Hello World")
-
+json = {"message": "Hello json"}
 app.get("/", hello)
+app.get("/json", getJSON)
 app.use("/public", express.static(middlewarePath))
 
-
+function getJSON(req, res){
+    if (process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message": "HELLO JSON"})
+    } else{
+        res.json({"message": "Hello json"})
+    }
+}
 function hello(req, res) {
     res.sendFile(absolutePath)
 }
