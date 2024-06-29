0866554ae72a98c1df61c9a4d5f37f6fe32374d3
Aidan Madden <aidanmadden22+github@gmail.com>
2024-06-29 22:53:58 +0000

    middleware

--- myApp.js
+++ myApp.js
@@ -1,16 +1,17 @@
 let express = require('express');
 const { log } = require('fcc-express-bground');
 let app = express();
+absolutePath = __dirname + '/views/index.html'
+middlewarePath = __dirname + '/public'
 console.log("Hello World")
 
 app.get("/", hello)
-absolutePath = __dirname + '/views/index.html'
+app.use("/public", express.static(middlewarePath))
+
 
 function hello(req, res) {
     res.sendFile(absolutePath)
 }
-
-
 
 
 
